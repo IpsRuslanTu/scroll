22dd93025b9150bf933ac695d98312f720253745
ruslan <r1tu@yandex.ru>
2024-02-13 00:07:20 +0300

    sv

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {useRef, useState} from "react";
+import useScroll from "./useScroll";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState([])
+    const [page, setPage] = useState(1)
+    const limit = 20
+
+    const parentRef = useRef()
+    const childRef = useRef()
+
+    useScroll(parentRef, childRef, () => fetchTodos(page, limit))
+
+    function fetchTodos(page, limit) {
+        fetch(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}&_page=${page}`)
+            .then(res => res.json())
+            .then(json => {
+                setTodos(prev => [...json, ...prev])
+                setPage(prev => prev + 1)
+                parentRef.current?.scrollBy(0, 30)
+            })
+    }
+
+    return (
+        <div ref={parentRef} style={{height: '70vh', background: "lightgray", overflow: "scroll"}}>
+            <div ref={childRef} style={{height: 20, background: 'green'}} />
+            {todos && todos.map(todo =>
+                <div key={todo.id} style={{padding: 30, border: '2px solid black'}}>
+                    {todo.id}. {todo.title}
+                </div>
+            )}
+
+        </div>
+    );
 }
 
 export default App;
